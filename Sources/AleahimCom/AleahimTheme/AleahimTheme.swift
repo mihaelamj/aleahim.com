5e979237537ca5d28aca46355ef68797f593db0b
Mihaela MJ <mihaelamj@me.com>
2024-04-05 02:41:00 +0200

    wip

--- Sources/AleahimCom/AleahimTheme/AleahimTheme.swift
+++ Sources/AleahimCom/AleahimTheme/AleahimTheme.swift
@@ -11,62 +11,70 @@ import Plot
 
 extension Theme where Site == AleahimCom {
     static var aleahimTheme: Self {
-        Theme(htmlFactory: AleahimHTMLFactory())
+        Theme(htmlFactory: AleahimHTMLFactory(),
+              resourcePaths: ["Resources/AleahimTheme/styles.css"])
     }
 }
 
 struct AleahimHTMLFactory: HTMLFactory {
-    typealias Site = AleahimCom
     
+    typealias Site = AleahimCom
     func makeIndexHTML(for index: Publish.Index, context: Publish.PublishingContext<AleahimCom>) throws -> Plot.HTML {
         HTML(
             .lang(context.site.language),
-            .head(for: context.site),
-            .body(
-                .grid(
-                    .header(for: context.site),
-                    .sidebar(for: context.site),
-                    .posts(
-                        for: context.allItems(
+            .head(for: index, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper {
+                    H1(index.title)
+                    Paragraph(context.site.description)
+                        .class("description")
+                    H2("Latest content")
+                    ItemList(
+                        items: context.allItems(
                             sortedBy: \.date,
                             order: .descending
                         ),
-                        on: context.site,
-                        title: "Recent posts"
-                    ),
-                    .footer(for: context.site)
-                )
-            )
+                        site: context.site
+                    )
+                }
+                SiteFooter()
+            }
         )
     }
     
     func makeSectionHTML(for section: Publish.Section<AleahimCom>, context: Publish.PublishingContext<AleahimCom>) throws -> Plot.HTML {
         HTML(
             .lang(context.site.language),
-            .head(for: context.site),
-            .body(
-                .grid(
-                    .header(for: context.site),
-                    .sidebar(for: context.site),
-                    .pageContent(.h1(.text(section.title))),
-                    .footer(for: context.site)
-                )
-            )
+            .head(for: section, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: section.id)
+                Wrapper {
+                    H1(section.title)
+                    ItemList(items: section.items, site: context.site)
+                }
+                SiteFooter()
+            }
         )
     }
     
     func makeItemHTML(for item: Publish.Item<AleahimCom>, context: Publish.PublishingContext<AleahimCom>) throws -> Plot.HTML {
         HTML(
             .lang(context.site.language),
-            .head(for: context.site),
+            .head(for: item, on: context.site),
             .body(
-                .grid(
-                    .header(for: context.site),
-                    .sidebar(for: context.site),
-                    .post(for: item, on: context.site),
-                    .footer(for: context.site)
-                )
-                
+                .class("item-page"),
+                .components {
+                    SiteHeader(context: context, selectedSelectionID: item.sectionID)
+                    Wrapper {
+                        Article {
+                            Div(item.content.body).class("content")
+                            Span("Tagged with: ")
+                            ItemTagList(item: item, site: context.site)
+                        }
+                    }
+                    SiteFooter()
+                }
             )
         )
     }
@@ -74,58 +82,149 @@ struct AleahimHTMLFactory: HTMLFactory {
     func makePageHTML(for page: Publish.Page, context: Publish.PublishingContext<AleahimCom>) throws -> Plot.HTML {
         HTML(
             .lang(context.site.language),
-            .head(for: context.site),
-            .body(
-                .grid(
-                    .header(for: context.site),
-                    .sidebar(for: context.site),
-                    .page(for: page, on: context.site),
-                    .footer(for: context.site)
-                )
-            )
+            .head(for: page, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper(page.body)
+                SiteFooter()
+            }
         )
     }
     
     func makeTagListHTML(for page: Publish.TagListPage, context: Publish.PublishingContext<AleahimCom>) throws -> Plot.HTML? {
         HTML(
             .lang(context.site.language),
-            .head(for: context.site),
-            .body(
-                .grid(
-                    .header(for: context.site),
-                    .sidebar(for: context.site),
-                    .pageContent(
-                        .tagList(for: page, on: context.site)
-                    ),
-                    .footer(for: context.site)
-                )
-            )
+            .head(for: page, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper {
+                    H1("Browse all tags")
+                    List(page.tags.sorted()) { tag in
+                        ListItem {
+                            Link(tag.string,
+                                 url: context.site.path(for: tag).absoluteString
+                            )
+                        }
+                        .class("tag")
+                    }
+                    .class("all-tags")
+                }
+                SiteFooter()
+            }
         )
     }
     
     func makeTagDetailsHTML(for page: Publish.TagDetailsPage, context: Publish.PublishingContext<AleahimCom>) throws -> Plot.HTML? {
         HTML(
             .lang(context.site.language),
-            .head(for: context.site),
-            .body(
-                .grid(
-                    .header(for: context.site),
-                    .sidebar(for: context.site),
-                    .posts(
-                        for: context.items(
+            .head(for: page, on: context.site),
+            .body {
+                SiteHeader(context: context, selectedSelectionID: nil)
+                Wrapper {
+                    H1 {
+                        Text("Tagged with ")
+                        Span(page.tag.string).class("tag")
+                    }
+
+                    Link("Browse all tags",
+                        url: context.site.tagListPath.absoluteString
+                    )
+                    .class("browse-all")
+
+                    ItemList(
+                        items: context.items(
                             taggedWith: page.tag,
                             sortedBy: \.date,
                             order: .descending
                         ),
-                        on: context.site,
-                        title: "\(page.tag.string.capitalized) posts"
-                    ),
-                    .footer(for: context.site)
-                )
-            )
+                        site: context.site
+                    )
+                }
+                SiteFooter()
+            }
         )
     }
-    
-    
-    
+}
+
+// MARK: Private Structs -
+
+private struct Wrapper: ComponentContainer {
+    @ComponentBuilder var content: ContentProvider
+
+    var body: Component {
+        Div(content: content).class("wrapper")
+    }
+}
+
+private struct SiteHeader<Site: Website>: Component {
+    var context: PublishingContext<Site>
+    var selectedSelectionID: Site.SectionID?
+
+    var body: Component {
+        Header {
+            Wrapper {
+                Link(context.site.name, url: "/")
+                    .class("site-name")
+
+                if Site.SectionID.allCases.count > 1 {
+                    navigation
+                }
+            }
+        }
+    }
+
+    private var navigation: Component {
+        Navigation {
+            List(Site.SectionID.allCases) { sectionID in
+                let section = context.sections[sectionID]
+
+                return Link(section.title,
+                    url: section.path.absoluteString
+                )
+                .class(sectionID == selectedSelectionID ? "selected" : "")
+            }
+        }
+    }
+}
+
+private struct ItemList<Site: Website>: Component {
+    var items: [Item<Site>]
+    var site: Site
+
+    var body: Component {
+        List(items) { item in
+            Article {
+                H1(Link(item.title, url: item.path.absoluteString))
+                ItemTagList(item: item, site: site)
+                Paragraph(item.description)
+            }
+        }
+        .class("item-list")
+    }
+}
+
+private struct ItemTagList<Site: Website>: Component {
+    var item: Item<Site>
+    var site: Site
+
+    var body: Component {
+        List(item.tags) { tag in
+            Link(tag.string, url: site.path(for: tag).absoluteString)
+        }
+        .class("tag-list")
+    }
+}
+
+private struct SiteFooter: Component {
+    var body: Component {
+        Footer {
+            Paragraph {
+                Text("Generated using ")
+                Link("Publish", url: "https://github.com/johnsundell/publish")
+            }
+            Paragraph {
+                Link("RSS feed", url: "/feed.rss")
+            }
+        }
+    }
 }
