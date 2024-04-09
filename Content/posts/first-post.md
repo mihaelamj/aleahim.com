a7288bb0f3455a1fd164be9b2f6b37cc28542f08
Mihaela MJ <mihaelamj@me.com>
2024-04-09 20:47:26 +0200

    wip

--- Content/posts/first-post.md
+++ Content/posts/first-post.md
@@ -6,3 +6,12 @@ tags: first, article
 # My first post
 
 My first post's text.
+
+```
+var i = 10
+while i > 0 {
+    print("\(i)...")
+    i -= 1
+}
+print("Blast off!")
+```
---
+++ Sources/AleahimCom/AleahimTheme/ContactMeItems.swift
@@ -0,0 +1,42 @@
+//
+//  ContactMeItems.swift
+//
+//
+//  Created by Mihaela MJ on 09.04.2024..
+//
+
+import Plot
+
+extension Node where Context == HTML.BodyContext {
+    static func contactList(_ contacts: [ContactMe]) -> Node {
+        .ul(
+            .forEach(contacts) { contact in
+                .li(
+                    .a(
+                        .href(contact.url),
+                        .text(contact.title),
+                        .class("icon \(contact.icon)") 
+                    )
+                )
+            }
+        )
+    }
+}
+
+
+import Publish
+
+struct ContactMeItemList<Site: Website>: Component {
+    var contacts: [ContactMe]
+    var site: Site
+    
+    var body: Component {
+        List(contacts) { contact in
+            Link(contact.title, url: contact.url)
+        }
+        .class("item-list")
+    }
+}
+
+
+
