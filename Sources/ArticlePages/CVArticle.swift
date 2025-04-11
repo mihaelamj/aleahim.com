396aa3547560c2fcdb607c6b934fb7e29c92f670
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-04-12 01:46:37 +0200

    updated dependancy

--- Sources/ArticlePages/CVArticle.swift
+++ Sources/ArticlePages/CVArticle.swift
@@ -1,10 +1,11 @@
 import Foundation
 import Ignite
 import CVBuilder
+import CVBuilderIgnite
 
 struct CVArticle: ArticlePage {
     var body: some HTML {
         let cv = CV.createForMihaela()
-        CVIgniteRenderer(cv: cv).body
+        IgniteRenderer(cv: cv).body
     }
 } 
