66341a7cd3af583e85cac5f7b5c5d995830c14a8
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2025-04-13 00:42:57 +0200

    wip

--- _samples/Ignite-main/Sources/Ignite/Framework/ElementTypes/HeadElement.swift
+++
@@ -1,9 +0,0 @@
-//
-// HeadElement.swift
-// Ignite
-// https://www.github.com/twostraws/Ignite
-// See LICENSE for license information.
-//
-
-/// A metadata element that can exist in the `HTMLHead` struct.
-public protocol HeadElement: HTML {}
--- _samples/Ignite-main/Tests/IgniteTesting/TestWebsitePackage/Package.swift
+++
@@ -1,2 +0,0 @@
-// Empty Package.swift file to stub source build directory in tests
-// See Publishing/Site.swift
